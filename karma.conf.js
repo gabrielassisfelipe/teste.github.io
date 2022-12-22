6ef1f285a339994fdc40dc4524eddfe423b1001c
Gabriel Assis Felipe <gabriel.felipe1997@hotmail.com>
2022-12-22 15:27:42 -0300

    alterado para dist ir pro git

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/teste'),
+      dir: require('path').join(__dirname, './coverage/teste.github.io/'),
       subdir: '.',
       reporters: [
         { type: 'html' },
